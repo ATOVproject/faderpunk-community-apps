08344d631ea2365ab6cec656de6c58590339d7cd
ArthurGibert <arthur@atov.de>
2026-09-01 15:28:46 +0200

    Use the canonical 16/CHANNELS expression for pool_size

--- apps/bassment.rs
+++ apps/bassment.rs
@@ -1517,7 +1517,7 @@ fn resolve_fill_hit(
     })
 }
 
-#[embassy_executor::task(pool_size = 16)]
+#[embassy_executor::task(pool_size = 16/CHANNELS)]
 pub async fn wrapper(app: App<CHANNELS>, exit_signal: &'static Signal<NoopRawMutex, bool>) {
     let param_store = ParamStore::<Params>::new(
         app.app_id,
