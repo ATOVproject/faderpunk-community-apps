2905e06cf37cf5cb2a2677d89a5ab4b9049ebfbb
ArthurGibert <arthur@atov.de>
2026-09-01 15:28:44 +0200

    Use the canonical 16/CHANNELS expression for pool_size

--- apps/harmonica.rs
+++ apps/harmonica.rs
@@ -653,7 +653,7 @@ async fn handle_midi_note_off(
     }
 }
 
-#[embassy_executor::task(pool_size = 16)]
+#[embassy_executor::task(pool_size = 16/CHANNELS)]
 pub async fn wrapper(app: App<CHANNELS>, exit_signal: &'static Signal<NoopRawMutex, bool>) {
     let param_store = ParamStore::<Params>::new(
         app.app_id,
